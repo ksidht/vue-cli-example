542054bc4a74bc74026e824aef25c65626232448
Siddharth <sid.maximess@gitlab.com>
2018-02-17 16:49:17 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld msg="welcome to the blockchain world" />
+    <Ether msg="Ethereum" />
+    <Form/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Ether from './components/Ether'
+import Form from './components/Form'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Ether,
+    Form
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Ether.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ price }}</h2>
+    <button @click="clicking()">Change Price</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ethereum',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      price: 200
+    }
+  },
+  methods: {
+    clicking () {
+      this.price += 300
+    }
+  }
+}
+/* eslint-disable */
+</script>
